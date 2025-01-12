fa19e9d696494f8678dbfd595a5708da2a4b7cf8
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2025-01-11 21:20:36 -0300

    feat: configuring the filter

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -13,25 +11,10 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var useCase = new RegisterExpenseUseCase();
+        var useCase = new RegisterExpenseUseCase();
 
-            var response = useCase.Execute(request);
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch (ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Errors);
-
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("unknow error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, "unknow error");
-        }
+        return Created(string.Empty, response);
     }
 }
