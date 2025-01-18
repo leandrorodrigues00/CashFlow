1498d2baba648bc1df5119ec91a6216157f7d211
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2025-01-17 21:50:31 -0300

    feat: database context

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "";
+        var serverVersion = new MySqlServerVersion(new Version(9, 1, 0));
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
