2bbe00f906f203cf37b7b7c55f88057bd307f017
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2025-01-19 11:27:26 -0300

    feat: implementing unit of work

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,13 +5,9 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=rootpassword;";
-        var serverVersion = new MySqlServerVersion(new Version(9, 1, 0));
 
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
