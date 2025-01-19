bfcbc481089479cf19d602df096ca3db172ac937
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2025-01-19 16:11:07 -0300

    feat: route to recover all expenses

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -22,5 +22,6 @@ public class AutoMapping : Profile
     private void EntityToResponse()
     {
         CreateMap<Expense, ResponseRegisteredExpenseJson>();
+        CreateMap<Expense, ResponseShortExpenseJson>();
     }
 }
--- src/CashFlow.Domain/Entities/Expense.cs
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -8,6 +8,6 @@ public class Expense
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public DateTime Date { get; set; }
-    public Decimal Amount { get; set; }
+    public decimal Amount { get; set; }
     public PaymentType PaymentType { get; set; }
 }
